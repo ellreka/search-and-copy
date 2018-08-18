aabc6f9626228aa6e0e7b279edf823b4fa251b5c
ellreka <kozakana333@gmail.com>
2018-08-18 10:53:55 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.contextMenus.create({
+  id: "search_and_copy",
+  title: "Googleで検索とコピー",
+  contexts: ["selection"],
+  type: "normal",
+});
+
+chrome.contextMenus.onClicked.addListener(function(info,tab){
+  var selectText = info.selectionText
+  var searchLink = `https://www.google.co.jp/search?q=${selectText}`
+  var textArea = document.createElement("textarea");
+  textArea.style.cssText = "position:absolute;left:-100%";
+  document.body.appendChild(textArea);
+  textArea.value = selectText;
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+  chrome.tabs.create({url: searchLink });
+})
